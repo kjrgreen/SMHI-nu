0176a44720ebac52b51feab7dc0e33a0b77df61b
kjrgreen <kevin.j.r.green@gmail.com>
2020-09-27 22:57:13 +0200

    Updates

---
+++ precache-manifest.92cf5608a24fc416452e22b1cef0625d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8f1ebb2ce92cf3f2b427713e1dc302d",
+    "url": "/SMHI-nu/index.html"
+  },
+  {
+    "revision": "efc0f05ea75d91e49d1e",
+    "url": "/SMHI-nu/static/css/main.7999bbbb.chunk.css"
+  },
+  {
+    "revision": "469c70020fbe07fce0af",
+    "url": "/SMHI-nu/static/js/2.b54a87fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SMHI-nu/static/js/2.b54a87fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efc0f05ea75d91e49d1e",
+    "url": "/SMHI-nu/static/js/main.cf0d3998.chunk.js"
+  },
+  {
+    "revision": "797fa5bf20ee4b5ae89c",
+    "url": "/SMHI-nu/static/js/runtime-main.f9c7f2eb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SMHI-nu/static/media/logo.5d5d9eef.svg"
+  }
+]);
